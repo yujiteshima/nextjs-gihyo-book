5e2d6208243dc4a3dbb1b1841c3d216d5d130a34
Yuji Teshima <yuji.teshima@gmail.com>
2022-08-12 23:20:03 +0900

    ESLintの設定

--- note.md
+++ note.md
@@ -1,3 +1,8 @@
 # メモ
 ## 動的ルーティング
 動的なルーティングを行うには、ファイル名にアンダースコアのプレフィックスしたものを作成。
+
+## ESLintの設定
+```bash
+npm install --save-dev prettier eslint typescript-eslint @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint-config-prettier eslint-plugin-react eslint-plugin-react-hooks eslint-plugin-import
+```s
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { createGlobalStyle } from 'styled-components';
 
@@ -24,19 +24,23 @@ const GlobalStyle = createGlobalStyle`
   ol, ul {
     list-style:none;
   }
-`
+`;
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <Head>
-        <meta key="charset" name='charset' content='utf-8' />
-        <meta key="viewport" name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=5' />
-        <meta property="og:locale" content='ja_JP' />
-        <meta property='og:type' content='website' />
+        <meta key="charset" name="charset" content="utf-8" />
+        <meta
+          key="viewport"
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=5"
+        />
+        <meta property="og:locale" content="ja_JP" />
+        <meta property="og:type" content="website" />
       </Head>
       <GlobalStyle />
-      <Component {...pageProps}/>
+      <Component {...pageProps} />
     </>
-  )
-}
-export default MyApp
+  );
+};
+export default MyApp;
